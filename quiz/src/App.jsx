7c10196a763b738e343c07928dfc05fd62a7314d
Marcus <69986201+marcus-hill@users.noreply.github.com>
2025-07-20 12:43:33 +0100

    update project

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,47 +1,76 @@
-import { useState, useEffect} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import HomeScreen from './pages/HomeScreen'
-import QuizScreen from './pages/QuizScreen'
-import ResultsScreen from './pages/ResultsScreen'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import HomeScreen from "./pages/HomeScreen";
+import htmlImage from "./assets/images/icon-html.svg";
+import cssImage from "./assets/images/icon-css.svg";
+import javascriptImage from "./assets/images/icon-js.svg";
+import accessibilityImage from "./assets/images/icon-accessibility.svg";
+import QuizScreen from "./pages/QuizScreen";
+import ResultsScreen from "./pages/ResultsScreen";
+import quizData from "./data.json";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   const [currentScreen, setCurrentScreen] = useState("home");
   const [quizStarted, setQuizStarted] = useState(false);
   const [currentCategory, setCurrentCategory] = useState(null);
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState("light");
 
   // Use Effect for Category Selection - Quiz Starting
   useEffect(() => {
     if (currentCategory) {
       setQuizStarted(true);
       setCurrentScreen("quiz");
-      console.log("Use Effect has been run")
     }
   }, [currentCategory]); // ← runs whenever currentCategory changes
 
   // Use Effect for Theme Selection
   useEffect(() => {
-    document.body.classList.remove('light', 'dark');
+    document.body.classList.remove("light", "dark");
     document.body.classList.add(theme);
   }, [theme]);
 
+  const returnCategoryImage = () => {
+    if (currentCategory.toLowerCase() === "html") {
+      return htmlImage;
+    } else if (currentCategory.toLowerCase() === "css") {
+      return cssImage;
+    } else if (currentCategory.toLowerCase() === "javascript") {
+      return javascriptImage;
+    } else if (currentCategory.toLowerCase() === "accessibility") {
+      return accessibilityImage;
+    }
+  };
+
+  const handleQuizCompleted = () => {};
 
   // useTheme(theme);
 
   return (
     <>
       <div>
-        { currentScreen === "home" && <HomeScreen currentCategory={currentCategory} setCurrentCategory={setCurrentCategory}></HomeScreen>}
-        { currentScreen === "quiz" && <QuizScreen currentCategory={currentCategory}></QuizScreen>}
-        { currentScreen === "results" && <ResultsScreen></ResultsScreen>}
-
+        <div className="topSection">
+          {quizStarted && (
+            <div className="categoryInformation">
+              <div className={"imageContainer " + currentCategory.toLowerCase()}>
+                <img className="categoryIcon" src={returnCategoryImage()} />
+              </div>
+              <p>{currentCategory}</p>
+            </div>
+          )}
+          <div className="theme-selector">
+            <p>Theme Placeholder</p>
+          </div>
+        </div>
+        {currentScreen === "home" && <HomeScreen currentCategory={currentCategory} setCurrentCategory={setCurrentCategory}></HomeScreen>}
+        {currentScreen === "quiz" && <QuizScreen currentCategory={currentCategory} handleQuizCompleted={handleQuizCompleted}></QuizScreen>}
+        {currentScreen === "results" && <ResultsScreen></ResultsScreen>}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- quiz/src/pages/QuizScreen.jsx
+++ quiz/src/pages/QuizScreen.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
+import quizData from "../data.json";
 
-const QuizScreen = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const QuizScreen = ({ currentCategory }) => {
+  const categoryObject = quizData["quizzes"].find((category) => category.title.toLowerCase() === currentCategory.toLowerCase());
+  const questions = categoryObject?.questions || []; // This makes the use of questions.map() safe - meaning it won't crash if the category isn't found
 
-export default QuizScreen
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [isAnswered, setIsAnswered] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(null);
+
+  return <div></div>;
+};
+
+export default QuizScreen;
